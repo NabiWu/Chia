78a0aca7e81813f872a916f3d7bb084f452f747c
NabiWu <bw1417@nyu.edu>
2022-02-24 13:58:50 -0600

    save swipe to FireStore - Finished debug of swiping card from last commit

--- GroupChia /Chia/Controllers/HomeController.swift
+++ GroupChia /Chia/Controllers/HomeController.swift
@@ -118,12 +118,50 @@ class HomeController: UIViewController, SettingsControllerDelegate, LoginControl
     
     var topCardView: CardView?
     
-    @objc fileprivate func handleLike() {
+    @objc func handleLike() {
+        saveSwipeToFirestore(didLike: 1)
         performSwipeAnimation(translation: 700, angle: 15)
     
     }
     
-    @objc fileprivate func handleDislike(){
+    fileprivate func saveSwipeToFirestore(didLike: Int) {
+        
+        guard let uid = Auth.auth().currentUser?.uid else {return}
+        guard let cardUID = topCardView?.cardViewModel.uid else {return}
+
+        let documentData = [
+            cardUID: didLike
+        ]
+        
+        Firestore.firestore().collection("swipes").document(uid).getDocument { snapshot, err in
+            if let err = err {
+                print("fail to fetch swipe document:", err)
+                return
+            }
+            
+            if snapshot?.exists == true {
+                Firestore.firestore().collection("swipes").document(uid).updateData(documentData) { err in
+                    if let err = err{
+                        print("Fail to save swipe data:", err)
+                        return
+                    }
+                    print("Successfully update swipe...")
+                }
+            } else {
+                Firestore.firestore().collection("swipes").document(uid).setData(documentData) { err in
+                    if let err = err{
+                        print("Fail to save swipe data:", err)
+                        return
+                    }
+                    print("Successfully saved swipe...")
+                }
+            }
+        }
+ 
+    }
+    
+    @objc func handleDislike(){
+        saveSwipeToFirestore(didLike: 0)
         performSwipeAnimation(translation: -700, angle: -15)
     }
     
