761e89b9a06d2f2ce3f87bfa267e742185844377
Chen <charles.chenyu1999@gmail.com>
2022-02-25 11:56:50 -0600

    impl PostItemController to handle post button

--- GroupChia /Chia/Controllers/PostItemController.swift
+++ GroupChia /Chia/Controllers/PostItemController.swift
@@ -6,84 +6,297 @@
 //
 
 import UIKit
+import Firebase
+import JGProgressHUD
+import SDWebImage
+protocol PostItemControllerDelegate {
+    func didSaveItems()
+}
 
-class PostItemController: UITableViewController {
-
+class PostItemController: UITableViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    var delegate: PostItemControllerDelegate?
+    
+    // instance properties
+    lazy var image1Button = createButton(selector: #selector(handleSelectPhoto))
+    lazy var image2Button = createButton(selector: #selector(handleSelectPhoto))
+    lazy var image3Button = createButton(selector: #selector(handleSelectPhoto))
+    
+    @objc func handleSelectPhoto(button: UIButton) {
+        print("Select photo with button:", button)
+        let imagePicker = CustomImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.imageButton = button
+        present(imagePicker, animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let selectedImage = info[.originalImage] as? UIImage
+        // how do i set the image on my buttons when I select a photo?
+        let imageButton = (picker as? CustomImagePickerController)?.imageButton
+        imageButton?.setImage(selectedImage?.withRenderingMode(.alwaysOriginal), for: .normal)
+        dismiss(animated: true)
+        
+        let filename = UUID().uuidString
+        let ref = Storage.storage().reference(withPath: "/images/\(filename)")
+        guard let uploadData = selectedImage?.jpegData(compressionQuality: 0.75) else { return }
+        
+        let hud = JGProgressHUD(style: .dark)
+        hud.textLabel.text = "Uploading image..."
+        hud.show(in: view)
+        ref.putData(uploadData, metadata: nil) { (nil, err) in
+            if let err = err {
+                hud.dismiss()
+                print("Failed to upload image to storage:", err)
+                return
+            }
+            
+            print("Finished uploading image")
+            ref.downloadURL(completion: { (url, err) in
+                
+                hud.dismiss()
+                
+                if let err = err {
+                    print("Failed to retrieve download URL:", err)
+                    return
+                }
+                
+                print("Finished getting download url:", url?.absoluteString ?? "")
+                
+                if imageButton == self.image1Button {
+                    self.item?.imageUrl1 = url?.absoluteString
+                } else if imageButton == self.image2Button {
+                    self.item?.imageUrl2 = url?.absoluteString
+                } else {
+                    self.item?.imageUrl3 = url?.absoluteString
+                }
+            })
+        }
+    }
+    
+    func createButton(selector: Selector) -> UIButton {
+        let button = UIButton(type: .system)
+        button.setTitle("Select Photo", for: .normal)
+        button.backgroundColor = .white
+        button.layer.cornerRadius = 8
+        button.addTarget(self, action: selector, for: .touchUpInside)
+        button.imageView?.contentMode = .scaleAspectFill
+        button.clipsToBounds = true
+        return button
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+        setupNavigationItems()
+        tableView = UITableView(frame: .zero, style: .grouped)
+        tableView.backgroundColor = UIColor(white: 0.95, alpha: 1)
+        tableView.tableFooterView = UIView()
+        tableView.keyboardDismissMode = .interactive
+        
+        fetchCurrentUser()
+        self.item = PostItem(name: "", description: "", price: 0)
     }
-
-    // MARK: - Table view data source
-
+    
+    var user: User?
+    var item: PostItem?
+    
+    fileprivate func fetchCurrentUser() {
+        Firestore.firestore().fetchCurrentUser { (user, err) in
+            if let err = err {
+                print("Failed to fetch user:", err)
+                return
+            }
+            self.user = user
+//            self.loadUserPhotos()
+            self.tableView.reloadData()
+        }
+    }
+// Don't need to load photes
+    
+//    fileprivate func loadUserPhotos() {
+//        if let imageUrl = user?.imageUrl1, let url = URL(string: imageUrl) {
+//            SDWebImageManager.shared().loadImage(with: url, options: .continueInBackground, progress: nil) { (image, _, _, _, _, _) in
+//                self.image1Button.setImage(image?.withRenderingMode(.alwaysOriginal), for: .normal)
+//            }
+//        }
+//        if let imageUrl = user?.imageUrl2, let url = URL(string: imageUrl) {
+//            SDWebImageManager.shared().loadImage(with: url, options: .continueInBackground, progress: nil) { (image, _, _, _, _, _) in
+//                self.image2Button.setImage(image?.withRenderingMode(.alwaysOriginal), for: .normal)
+//            }
+//        }
+//        if let imageUrl = user?.imageUrl3, let url = URL(string: imageUrl) {
+//            SDWebImageManager.shared().loadImage(with: url, options: .continueInBackground, progress: nil) { (image, _, _, _, _, _) in
+//                self.image3Button.setImage(image?.withRenderingMode(.alwaysOriginal), for: .normal)
+//            }
+//        }
+//    }
+    
+    lazy var header: UIView = {
+        let header = UIView()
+        header.addSubview(image1Button)
+        let padding: CGFloat = 16
+        image1Button.anchor(top: header.topAnchor, leading: header.leadingAnchor, bottom: header.bottomAnchor, trailing: nil, padding: .init(top: padding, left: padding, bottom: padding, right: 0))
+        image1Button.widthAnchor.constraint(equalTo: header.widthAnchor, multiplier: 0.45).isActive = true
+        
+        let stackView = UIStackView(arrangedSubviews: [image2Button, image3Button])
+        stackView.axis = .vertical
+        stackView.distribution = .fillEqually
+        stackView.spacing = padding
+        
+        header.addSubview(stackView)
+        stackView.anchor(top: header.topAnchor, leading: image1Button.trailingAnchor, bottom: header.bottomAnchor, trailing: header.trailingAnchor, padding: .init(top: padding, left: padding, bottom: padding, right: padding))
+        return header
+    }()
+    
+    class HeaderLabel: UILabel {
+        override func drawText(in rect: CGRect) {
+            super.drawText(in: rect.insetBy(dx: 16, dy: 0))
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        if section == 0 {
+            return header
+        }
+        let headerLabel = HeaderLabel()
+        switch section {
+        case 1:
+            headerLabel.text = "Name"
+        case 2:
+            headerLabel.text = "Description"
+        default:
+            headerLabel.text = "Price"
+        }
+        headerLabel.font = UIFont.boldSystemFont(ofSize: 16)
+        return headerLabel
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        if section == 0 {
+            return 300
+        }
+        return 40
+    }
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 4
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return section == 0 ? 0 : 1
     }
-
-    /*
+    
+//    @objc fileprivate func handleMinPriceChange(slider: UISlider){
+//
+//        evaluateMinMax()
+//    }
+//
+//    @objc fileprivate func handleMaxPriceChange(slider: UISlider){
+//        evaluateMinMax()
+//    }
+//
+//
+//    fileprivate func evaluateMinMax() {
+//        guard let priceRangeCell = tableView.cellForRow(at: [3, 0]) as? PriceRangeCell else { return }
+//        let minValue = Int(priceRangeCell.minSlider.value)
+//        var maxValue = Int(priceRangeCell.maxSlider.value)
+//        maxValue = max(minValue, maxValue)
+//        priceRangeCell.maxSlider.value = Float(maxValue)
+//        priceRangeCell.minLabel.text = "Min \(minValue)"
+//        priceRangeCell.maxLabel.text = "Max \(maxValue)"
+//
+//        user?.minSeekingPrice = minValue
+//        user?.maxSeekingPrice = maxValue
+//    }
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+//        if indexPath.section == 3 {
+//            let priceRangeCell = PriceRangeCell(style: .default, reuseIdentifier: nil)
+//            priceRangeCell.minSlider.addTarget(self, action: #selector(handleMinPriceChange), for: .valueChanged)
+//            priceRangeCell.maxSlider.addTarget(self, action: #selector(handleMaxPriceChange), for: .valueChanged)
+//
+//            let minPrice = user?.minSeekingPrice ?? SettingsController.defaultMinSeekingPrice
+//            let maxPrice = user?.maxSeekingPrice ?? SettingsController.defaultMaxSeekingPrice
+//
+//            priceRangeCell.minLabel.text = "Min \(minPrice)"
+//            priceRangeCell.maxLabel.text = "Max \(maxPrice)"
+//            priceRangeCell.minSlider.value = Float(minPrice)
+//            priceRangeCell.maxSlider.value = Float(maxPrice)
+//            return priceRangeCell
+//        }
+        
+        let cell = SettingsCell(style: .default, reuseIdentifier: nil)
+        
+        switch indexPath.section {
+        case 1:
+            cell.textField.placeholder = "Enter Name"
+            cell.textField.addTarget(self, action: #selector(handleNameChange), for: .editingChanged)
+        case 2:
+            cell.textField.placeholder = "Enter Description"
+            cell.textField.addTarget(self, action: #selector(handleDescriptionChange), for: .editingChanged)
+        default:
+            cell.textField.placeholder = "Enter Price"
+            cell.textField.addTarget(self, action: #selector(handlePriceChange), for: .editingChanged)
+        }
+        
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    @objc fileprivate func handleNameChange(textField: UITextField) {
+        self.item?.name = textField.text
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    
+    @objc fileprivate func handleDescriptionChange(textField: UITextField) {
+        self.item?.description = textField.text
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
+    
+    @objc fileprivate func handlePriceChange(textField: UITextField) {
+        self.item?.price = Int(textField.text ?? "")
     }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
+    
+    fileprivate func setupNavigationItems() {
+        navigationItem.title = "Settings"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(handleCancel))
+        navigationItem.rightBarButtonItems = [
+            UIBarButtonItem(title: "Save", style: .plain, target: self, action: #selector(handleSave))
+        ]
     }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    @objc fileprivate func handleSave() {
+        print("Saving our settings data into Firestore")
+        guard let uid = Auth.auth().currentUser?.uid else { return }
+        let docData: [String: Any] = [
+            "uid": user?.uid as Any,
+            "name": item?.name ?? "",
+            "imageUrl1": item?.imageUrl1 ?? "",
+            "imageUrl2": item?.imageUrl2 ?? "",
+            "imageUrl3": item?.imageUrl3 ?? "",
+            "description": item?.description ?? "",
+            "price": item?.price as Any
+        ]
+        
+        let hud = JGProgressHUD(style: .dark)
+        hud.textLabel.text = "Saving settings"
+        hud.show(in: view)
+        Firestore.firestore().collection("items").document(UUID().uuidString).setData(docData) { (err) in
+            hud.dismiss()
+            if let err = err {
+                print("Failed to save user settings:", err)
+                return
+            }
+            
+            print("Finished saving user info")
+            self.dismiss(animated: true) {
+                print("Dismissal complete")
+                self.delegate?.didSaveItems()
+            }
+        }
     }
-    */
+    
+    @objc fileprivate func handleCancel() {
+        dismiss(animated: true)
+    }
+
 
 }
