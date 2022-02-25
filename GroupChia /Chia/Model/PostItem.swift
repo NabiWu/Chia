5c4441b3a3170c5dab1ef46bb231601adac3457f
Chen <charles.chenyu1999@gmail.com>
2022-02-25 11:56:00 -0600

    impl PostItem Model

---
+++ GroupChia /Chia/Model/PostItem.swift
@@ -0,0 +1,27 @@
+//
+//  PostItem.swift
+//  Chia
+//
+//  Created by Chen Yu on 2022/2/25.
+//
+
+import UIKit
+
+struct PostItem: ProducesCardViewModel {
+    var name: String?
+    var description: String?
+    var price: Int?
+    
+    var imageUrl1: String?
+    var imageUrl2: String?
+    var imageUrl3: String?
+    
+    func toCardViewModel() -> CardViewModel {
+        let attributedString = NSMutableAttributedString(string: description ?? "", attributes: [.font: UIFont.systemFont(ofSize: 34, weight: .heavy)])
+        
+        attributedString.append(NSAttributedString(string: "\n" + String(price ?? 0), attributes: [.font: UIFont.systemFont(ofSize: 24, weight: .bold)]))
+        
+//        return CardViewModel(imageNames: [name], attributedString: attributedString, textAlignment: .center)
+        return CardViewModel(imageNames: ["name"], attributedString: attributedString, textAlignment: .center)
+    }
+}
